caa2cae6aad2c5935b0571f9c686bd4e635ebc1d
Your Name <emmanuel.diop@gmail.com>
2024-02-26 21:02:54 +0100

    repositionnement du footer

--- PHP/inc/footer.tpl.php
+++ PHP/inc/footer.tpl.php
@@ -1,7 +1,8 @@
 <footer class="footer">
-            <span class="footer__span">Emmanuel Diop - Copyright &#169; - 2024</span>
-        </footer>
-    </div>
+    <span class="footer__span">Emmanuel Diop - Copyright &#169; - 2024</span>
+</footer>
+</div>
 
 </body>
-</html>    
+
+</html>
